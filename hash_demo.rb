04dbd9ae5398e1eab44616ea1937d686b74430c4
mccordt <mccordt@nku.edu>
2017-09-01 09:49:23 -0400

    Update hash_demo.rb

--- hash_demo.rb
+++ hash_demo.rb
@@ -15,7 +15,8 @@ hash[100] << 'B'
 
 hash.each do |key, val|       # iterate over the Hash
   print "#{key}\t "           # use String interpolation for output
-  val.each { |element| print "#{element} " }      # iterate over the Array that is the value stored
+  # iterate over the Array that is the value stored
+  val.each { |element| print "#{element} " }      
   puts       # print a blank line
 end
 
