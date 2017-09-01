10d95fd7ce0a7bbd1608333b057e2052cc729e0c
mccordt <mccordt@nku.edu>
2017-09-01 09:48:05 -0400

    Update hash_demo.rb

--- hash_demo.rb
+++ hash_demo.rb
@@ -3,9 +3,9 @@
 hash = Hash.new
 
 hash[99] = []   # store an empty Array object with key 99
-hash.store(100, [])
+hash.store(100, [])  # alternate way to store an Array object with key 100
 
-hash[99] << 'a'
+hash[99] << 'a'   # push onto the Array at location hash[99]
 hash[99] << 'b'
 hash[99] << 'c'
 
@@ -13,9 +13,9 @@ hash[100] << 'A'
 hash[100] << 'B'
 
 
-hash.each do |key, val|
-  print "#{key}\t "
-  val.each { |element| print "#{element} " } 
-  puts   
+hash.each do |key, val|       # iterate over the Hash
+  print "#{key}\t "           # use String interpolation for output
+  val.each { |element| print "#{element} " }      # iterate over the Array that is the value stored
+  puts       # print a blank line
 end
 
